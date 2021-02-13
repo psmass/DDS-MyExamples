0608efd37e2d2fc5fe527f56719ae460b59e84f4
Paul Schmitt <42389607+psmass@users.noreply.github.com>
2021-02-13 13:52:32 -0500

    Add C and Modern CPP variants

---
+++ ShapesCompiledC/c/README_x64Darwin17clang9.0.txt
@@ -0,0 +1,33 @@
+Welcome to your first Connext DDS example! 
+
+This example was generated for architecture x64Darwin17clang9.0, using the
+data type ShapeTypeExtended from ShapeType.idl.
+This example builds two applications, named ShapeType_publisher and
+ShapeType_subscriber.
+
+To Build this example:
+======================
+ 
+From your command shell, type:
+> make -f makefile_ShapeType_x64Darwin17clang9.0
+This command will build a release executable.
+ 
+To build a debug version instead:
+> make -f makefile_ShapeType_x64Darwin17clang9.0 DEBUG=1
+
+To Modify the Data:
+===================
+To modify the data being sent, edit the ShapeType_publisher.c
+file where it says:
+/* Modify the instance to be written here */
+
+To Run this Example:
+====================
+Make sure you are in the directory where the USER_QOS_PROFILES.xml file was
+generated (the same directory this README file is in).
+Run /Applications/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Darwin17clang9.0.bash
+to make sure the Connext libraries are in the path, especially if you opened
+a new command prompt window.
+Run the publishing or subscribing application by typing:
+> objs/x64Darwin17clang9.0/ShapeType_publisher <domain_id> <sample_count>
+> objs/x64Darwin17clang9.0/ShapeType_subscriber <domain_id> <sample_count>
---
+++ ShapesCompiledC/c/ShapeTypeSupport.h
@@ -0,0 +1,83 @@
+
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from ShapeType.idl using "rtiddsgen".
+The rtiddsgen tool is part of the RTI Connext distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the RTI Connext manual.
+*/
+
+#ifndef ShapeTypeSupport_434673741_h
+#define ShapeTypeSupport_434673741_h
+
+/* Uses */
+#include "ShapeType.h"
+
+#ifndef ndds_c_h
+#include "ndds/ndds_c.h"
+#endif
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+
+#endif
+
+/* ========================================================================= */
+/**
+Uses:     T
+
+Defines:  TTypeSupport, TDataWriter, TDataReader
+
+Organized using the well-documented "Generics Pattern" for
+implementing generics in C and C++.
+*/
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
+
+#endif
+
+DDS_TYPESUPPORT_C(ShapeTypeTypeSupport, ShapeType);
+DDS_DATAWRITER_WITH_DATA_CONSTRUCTOR_METHODS_C(ShapeTypeDataWriter, ShapeType);
+DDS_DATAREADER_C(ShapeTypeDataReader, ShapeTypeSeq, ShapeType);
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
+/* ========================================================================= */
+/**
+Uses:     T
+
+Defines:  TTypeSupport, TDataWriter, TDataReader
+
+Organized using the well-documented "Generics Pattern" for
+implementing generics in C and C++.
+*/
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
+
+#endif
+
+DDS_TYPESUPPORT_C(ShapeTypeExtendedTypeSupport, ShapeTypeExtended);
+DDS_DATAWRITER_WITH_DATA_CONSTRUCTOR_METHODS_C(ShapeTypeExtendedDataWriter, ShapeTypeExtended);
+DDS_DATAREADER_C(ShapeTypeExtendedDataReader, ShapeTypeExtendedSeq, ShapeTypeExtended);
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
+
+#endif  /* ShapeTypeSupport_434673741_h */
+
