7d08f6bfdd4016c799fa1ced24d9f71fa0da156b
Paul Schmitt <42389607+psmass@users.noreply.github.com>
2022-08-17 11:01:43 -0400

    Added Docker build files for ShapesCompiledC11

--- ShapesCompiledC11/cxx/ShapeTypePlugin.cxx
+++ ShapesCompiledC11/cxx/ShapeTypePlugin.cxx
@@ -4,7 +4,7 @@
 WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
 
 This file was generated from ShapeType.idl
-using RTI Code Generator (rtiddsgen) version 3.1.1.
+using RTI Code Generator (rtiddsgen) version 3.1.0.
 The rtiddsgen tool is part of the RTI Connext DDS distribution.
 For more information, type 'rtiddsgen -help' at a command shell
 or consult the Code Generator User's Manual.
